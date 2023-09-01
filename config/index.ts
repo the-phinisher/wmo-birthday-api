d30319e1a8626f73a610462f12c75df7a2c07608
Gandhar Vabale <vabalegandhar@gmail.com>
2023-09-01 20:50:45 +0530

    Restructured configuration

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,14 @@
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 dotenv.config()
 
-export const PORT = process.env.PORT
-export const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(
+	process.env.MONGODB_URI
+		? process.env.MONGODB_URI
+		: "mongodb://localhost:27017",
+)
+
+export default {
+	PORT: process.env.PORT,
+	MONGODB_URI: process.env.MONGODB_URI,
+}
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 require("dotenv").config()
-import express from "express"
 import bodyParser from "body-parser"
-import { PORT } from "./Config"
+import express from "express"
+import config from "./Config"
 import apiRouter from "./Routers/apiRouter"
 const app = express()
 
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use("/", apiRouter)
 
-app.listen(PORT, () => {
-	console.log("Server started at " + PORT)
+app.listen(config.PORT, () => {
+	console.log("Server started at " + config.PORT)
 })
